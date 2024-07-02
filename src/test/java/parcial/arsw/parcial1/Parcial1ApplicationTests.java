4b5899531c81a812f505e30d23bb1e14ccef15f2
Parralol <110953563+Parralol@users.noreply.github.com>
2024-07-02 10:46:11 -0500

    added content

---
+++ src/test/java/parcial/arsw/parcial1/Parcial1ApplicationTests.java
@@ -0,0 +1,13 @@
+package parcial.arsw.parcial1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
